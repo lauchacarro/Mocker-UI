6926bbeb07fb552824e52fcf58b6d84b20c315e1
lautaro <lautaroecarro@gmail.com>
2019-10-14 00:03:40 -0300

    on start app send request to wake up service

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Loading from './components/Loading'
 import './App.css'
+import { config } from './components/Api/config'
 const Home = lazy(() => import('./pages/Home'));
 
 const App = () => {
+  fetch(config.Url + "api")
+  
   return (
     <Suspense fallback={<Loading />}>
       <Router basename={process.env.PUBLIC_URL}>>
